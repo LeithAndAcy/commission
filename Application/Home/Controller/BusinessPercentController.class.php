2e604e4454d26423a75194339bc80776738d69f6
linji <374083764@qq.com>
2019-03-02 22:12:10 +0800

    2019-03-02 1、  短米底价上浮比例：这个比例的获取条件修改为： 只要是存货的属性为“现货”（表体自定义项12  cDefine33）的，都不考虑这个比例（这个条件是新增的）； 调整为：只要是存货的属性为“现货-备货”、“现货-裁线”、“外购”、“线束”、“代卖”（表体自定义项12  cDefine33）的，都不考虑这个比例

--- Application/Home/Controller/BusinessPercentController.class.php
+++ Application/Home/Controller/BusinessPercentController.class.php
@@ -317,7 +317,7 @@ class BusinessPercentController extends Controller {
 
 			foreach ($price_float_ratio as $kkkk => $vvvv) {
 				if ($vvvv['classification_id'] == $contact_detail[$key]['classification_id'] && $vvvv['low_price'] <= $contact_detail[$key]['cost_price'] && $vvvv['high_price'] > $contact_detail[$key]['cost_price'] && $vvvv['low_length'] <= $contact_detail[$key]['sale_quantity'] && $vvvv['high_length'] > $contact_detail[$key]['sale_quantity']) {
-					if($value['inStore'] == '现货-备货' || $value['inStore'] == '现货-裁线' || $value['inStore'] == '线束'){
+					if($value['inStore'] == '外购' || $value['inStore'] == '现货-备货' || $value['inStore'] == '现货-裁线' || $value['inStore'] == '线束' || $value['inStore'] == '代卖'){
 						$vvvv['ratio'] = 0;
 					}	
 					$arr_ratio[$key]['float_price'] = ($vvvv['ratio'] * 0.01 + $area_price_float_ratio) * $contact_detail[$key]['cost_price'];
